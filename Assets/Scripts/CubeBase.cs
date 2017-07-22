c0f442408bbe37b4e4070c2f98bc6e2ece58e2be
Topher Wilson <tophermade@gmail.com>
2017-07-21 23:01:04 -0600

    episode one complete

---
+++ Assets/Scripts/CubeBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBase : MonoBehaviour {
+
+	public GameObject indicator;
+	private GameObject indicatorActual;
+	public GameObject baseObject;
+	public GameObject[] mountPoints;
+
+
+	public float jumpTime = .8f;
+
+
+
+	void SetupCube(){
+		Transform tempMount = mountPoints[Random.Range(0, mountPoints.Length)].transform;
+		indicatorActual = Instantiate(indicator, tempMount.position,Quaternion.identity);
+		indicatorActual.transform.parent = tempMount;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		SetupCube();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Lumbergh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lumbergh : MonoBehaviour {
+
+
+	public GameObject cube;
+	public GameObject cubeParent;
+
+
+
+	void BlockSpawn(){
+		GameObject tempIndicator = GameObject.Find("Indicator(Clone)");
+		GameObject newCube = Instantiate(cube, tempIndicator.transform.position, Quaternion.identity);
+		newCube.transform.parent = cubeParent.transform;
+		//Debug.Log(tempIndicator.transform.position);
+	}
+	
+
+	void SetupWorld(){
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		SetupWorld();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			BlockSpawn();
+		}
+	}	
+}
