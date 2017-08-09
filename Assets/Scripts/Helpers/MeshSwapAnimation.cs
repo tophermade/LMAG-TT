6377f6fd43b90c7e050547974d6d9ac655e7e483
Topher Wilson <tophermade@gmail.com>
2017-08-09 13:13:52 -0600

    added sws and birds, correlated paths

---
+++ Assets/Scripts/Helpers/MeshSwapAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSwapAnimation : MonoBehaviour {
+
+	public GameObject[] states;
+	public float timeBetweenStates = .25f;
+	
+	private int currentState = 0;
+	private float lastStateChangeTime = 0f;
+
+	public void UpdateState(){
+		if(Time.time > lastStateChangeTime + timeBetweenStates){
+			states[currentState].SetActive(false);
+			if(currentState == states.Length - 1){
+				currentState = 0;
+			} else {
+				currentState++;
+			}
+			states[currentState].SetActive(true);
+			lastStateChangeTime = Time.time;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateState();		
+	}
+}
